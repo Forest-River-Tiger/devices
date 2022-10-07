597bafe77e947f1cdfd2465ca862dcc9b6a7a90f
taiga <motantanmen.github@gmail.com>
2022-10-07 23:57:58 +0900

    リスト編集

--- lib/Third_page.dart
+++ lib/Third_page.dart
@@ -15,39 +15,40 @@ class ThirdPage extends StatelessWidget {
         ),
         // Containerは箱(指定しなければ何もなし)
         // ListViewの場合、スクロールすることも可能
-        // body: ListView(
-        //   padding: const EdgeInsets.all(5),
-        //   children: <Widget>[
-        //     Container(
-        //       height: 100,
-        //       color: Colors.amber[600],
-        //       child: const Center(child: Text('Entry A')),
-        //     ),
-        //     Container(
-        //       height: 50,
-        //       color: Colors.amber[500],
-        //       child: const Center(child: Text('Entry B')),
-        //     ),
-        //     Container(
-        //       height: 25,
-        //       color: Colors.amber[100],
-        //       child: const Center(child: Text('Entry C')),
-        //     ),
-        //   ],
-        // ),
+        body: ListView(
+          padding: const EdgeInsets.all(5),
+          children: <Widget>[
+            Container(
+              height: 100,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 25,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ],
+        ),
 
         // ListView.builder
-        body: ListView.separated(
-          // 変数entriesの数
-          itemCount: entries.length,
-          // entriesの数だけ周回
-          itemBuilder: (BuildContext context, int index) {
-            // Textの中に変数を入れる場合は「${}で囲む」
-            return Center(child: Text('Entry ${entries[index]}'));
-          },
-          separatorBuilder: (BuildContext context, int index) => const Divider(
-            color: Colors.black,
-          ),
-        ));
+        // body: ListView.separated(
+        //   // 変数entriesの数
+        //   itemCount: entries.length,
+        //   // entriesの数だけ周回
+        //   itemBuilder: (BuildContext context, int index) {
+        //     // Textの中に変数を入れる場合は「${}で囲む」
+        //     return Center(child: Text('Entry ${entries[index]}'));
+        //   },
+        //   separatorBuilder: (BuildContext context, int index) => const Divider(
+        //     color: Colors.black,
+        //   ),
+        // )
+        );
   }
 }
