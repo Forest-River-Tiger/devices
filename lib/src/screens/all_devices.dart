5c303875a0c995e2691263a221a8ef1fd233a4f9
taiga <motantanmen.github@gmail.com>
2022-10-15 18:26:30 +0900

    モーダル追加

--- lib/src/screens/all_devices.dart
+++ lib/src/screens/all_devices.dart
@@ -3,18 +3,40 @@ import 'package:flutter/material.dart';
 class AllDevicePage extends StatelessWidget {
   const AllDevicePage({Key? key}) : super(key: key);
 
-  // final List<String> entries = <String>['A', 'B', 'C', 'Taiga'];
-  // final List<int> colorCodes = <int>[600, 500, 100];
-
   @override
   Widget build(BuildContext context) {
     // Scaffoldは大元の画面
     return Scaffold(
         // AppBarはnavigationbar
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed:() => {
+              showModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                isScrollControlled: true,
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
+                    margin: EdgeInsets.only(top: 64),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  );
+                }
+              ),
+            },
+          ),
+        ],
         automaticallyImplyLeading: false,
         backgroundColor: Colors.red.withOpacity(0.5),
-        title: const Text('デバイス一覧', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        title: const Text('デバイス一覧', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
       ),
         // Containerは箱(指定しなければ何もなし)
         // ListViewの場合、スクロールすることも可能
@@ -22,19 +44,24 @@ class AllDevicePage extends StatelessWidget {
           padding: const EdgeInsets.all(5),
           children: <Widget>[
             Container(
-              height: 100,
-              color: Colors.amber[600],
+              height: 50,
+              color: Colors.grey[500],
               child: const Center(child: Text('Entry A')),
             ),
             Container(
               height: 50,
-              color: Colors.amber[500],
+              color: Colors.grey[500],
               child: const Center(child: Text('Entry B')),
             ),
             Container(
-              height: 25,
-              color: Colors.amber[100],
+              height: 50,
+              color: Colors.grey[500],
               child: const Center(child: Text('Entry C')),
+            ),
+            Divider(
+              height: 2,
+              thickness: 3,
+              color: Colors.black,
             ),
           ],
         ),
