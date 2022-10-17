04bfbde405555082fa7a1cb8bb6470737cc8e179
taiga <motantanmen.github@gmail.com>
2022-10-18 00:24:04 +0900

    デバイス追加画面作成

--- lib/src/screens/add_device.dart
+++ lib/src/screens/add_device.dart
@@ -5,12 +5,72 @@ class AddDevicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // ボタン押下時の処理
-          },
-          child: const Text('デバイス追加'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 50),
+                TextFormField(
+                  // テキストラベル
+                  decoration: InputDecoration(labelText: '端末名'),
+                  onChanged: (String value) {
+                  },
+                ),
+                const SizedBox(height: 50),
+                TextFormField(
+                  // テキストラベル
+                  decoration: InputDecoration(labelText: "Ver"),
+                  onChanged: (String value) {
+                  },
+                ),
+                const SizedBox(height: 50),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => {},
+                      child: Text('キャンセル'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.redAccent),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {},
+                      child: Text('デバイス追加'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                      ),
+                    ),
+                  ],
+                )
+                // Row(
+                //   children: [
+                //     ElevatedButton(
+                //       onPressed: () => {
+
+                //       },
+                //       child: Text('ボタン1'),
+                //     ),
+                //     ElevatedButton(
+                //       onPressed: () => {
+
+                //       },
+                //       child: Text('ボタン2'),
+                //     ),
+                //   ],
+                // )
+                // ElevatedButton(
+                //   onPressed: () => {
+                //     // ボタン押下後の処理
+                //   },
+                //   child: Text('デバイス追加'),
+                // ),
+              ],
+            ),
+          ),
         ),
       ),
     );
